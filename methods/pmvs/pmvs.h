ae5cabafb42fd38b2358b6875bb2d2db41472754
manlito <manlito@gmail.com>
2016-02-11 23:22:57 -0600

    Added library dependencies and base files for multiple modules

---
+++ methods/pmvs/pmvs.h
@@ -0,0 +1,14 @@
+#ifndef DENSEPOINTS_PMVS_PMVS
+#define DENSEPOINTS_PMVS_PMVS
+
+namespace DensePoints {
+  class PMVS {
+  public:
+
+    void AddCamera();
+
+  protected:
+  };
+}
+
+#endif DENSEPOINTS_PMVS_PMVS
